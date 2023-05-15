024c79d6db041d3c46d695eaab2ea1c467089439
tltl111 <tlafeber@outlook.com>
2023-05-15 13:23:52 +0200

    Testchange

--- src/IntroductieClass.java
+++ src/IntroductieClass.java
@@ -1,7 +1,5 @@
 public class IntroductieClass {
     public IntroductieClass(String naam) {
-//        this.naam = naam;
-//        System.out.print("hoi");
         System.out.print(naam);
     }
 }
