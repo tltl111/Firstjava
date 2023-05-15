2ce45a7e149eb295faa54e88ec28283cbfb844d8
tltl111 <tlafeber@outlook.com>
2023-05-15 13:28:03 +0200

    Testchange

--- src/IntroductieClass.java
+++ src/IntroductieClass.java
@@ -1,5 +1,9 @@
 public class IntroductieClass {
     public IntroductieClass(String naam) {
-        System.out.print(naam);
+        System.out.print("Hi ");
+        System.out.println(naam);
+        System.out.print("I have an age\n");
+        System.out.print("this is a line with print and escape n\n");
+        System.out.println("this is a line with println");
     }
 }
