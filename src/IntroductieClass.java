309fa1140f05245438b1bb36b64e6d928a9c8f76
tltl111 <tlafeber@outlook.com>
2023-05-15 12:56:34 +0200

    Initial commit

---
+++ src/IntroductieClass.java
@@ -0,0 +1,7 @@
+public class IntroductieClass {
+    public IntroductieClass(String naam) {
+        this.naam = "naam";
+        System.out.print("hoi");
+        System.out.print(naam);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        IntroductieClass tijdelijk = new IntroductieClass("Tom");
+        IntroductieClass tijdelijk2 = new IntroductieClass("randomnaam");
+
+    }
+}
