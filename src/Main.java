ce7e061ed99e55135c8127cf96a1c3af49289bb3
tltl111 <tlafeber@outlook.com>
2023-05-15 13:19:04 +0200

    Testchange

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args)
     {
         IntroductieClass tijdelijk = new IntroductieClass("Tom");
-        IntroductieClass tijdelijk2 = new IntroductieClass("randomnaam");
+//        IntroductieClass tijdelijk2 = new IntroductieClass("randomnaam");
 
     }
 }
