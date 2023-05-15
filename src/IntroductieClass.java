8eeee4ddc9994ed21f5bed37c1f639d5d44fa345
tltl111 <tlafeber@outlook.com>
2023-05-15 15:47:03 +0200

    Testchange

--- src/IntroductieClass.java
+++ src/IntroductieClass.java
@@ -1,8 +1,40 @@
 public class IntroductieClass {
-    public IntroductieClass(String naam) {
-        System.out.print("Hi ");
-        System.out.println(naam + "\nI have an age");
-        System.out.print("this is a line with print and escape n\n");
-        System.out.println("this is a line with println");
+    private String naam;
+    private int leeftijd;
+    private String geslacht;
+    public IntroductieClass(String naam, int leeftijd, String geslacht) {
+        this.naam = naam;
+        this.leeftijd = leeftijd;
+        this.geslacht = geslacht;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public int getLeeftijd() {
+        return leeftijd;
+    }
+
+    public void setLeeftijd(int leeftijd) {
+        this.leeftijd = leeftijd;
+    }
+
+    public String getGeslacht() {
+        return geslacht;
+    }
+
+    public void setGeslacht(String geslacht) {
+        this.geslacht = geslacht;
+    }
+
+    public void voorstellen() {
+        System.out.println("Naam: " + naam);
+        System.out.println("Leeftijd: " + leeftijd);
+        System.out.println("Geslacht: " + geslacht);
     }
 }
