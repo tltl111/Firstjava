c4363bad23f1a688d47998b94c547446866855c7
tltl111 <tlafeber@outlook.com>
2023-05-25 14:11:42 +0200

    Added a random starting status to the lamp

--- src/lamp.java
+++ src/lamp.java
@@ -1,17 +1,20 @@
+import java.util.Random;
+
+
 public class lamp {
     private boolean lampStatus;
 
-    public lamp() {
+    public lamp(boolean lampStatus) {
         this.lampStatus = lampStatus;
     }
 
     public void schakelLampOm() {
         if (this.lampStatus == true) {
-            lampStatus = false;
+            this.lampStatus = false;
             System.out.println("Lamp van aan naar uit geschakeld");
             printLampStatus();
         } else {
-            lampStatus = true;
+            this.lampStatus = true;
             System.out.println("Lamp van uit naar naar geschakeld");
             printLampStatus();
         }
@@ -20,17 +23,23 @@ public class lamp {
     public void zetLampAan() {
         if (this.lampStatus == true) {
             System.out.println("Lamp staat al aan");
+            printLampStatus();
         } else {
-            lampStatus = true;
+            this.lampStatus = true;
             System.out.println("Lamp aangezet");
             printLampStatus();
         }
     }
 
     public void zetLampUit() {
-        lampStatus = false;
-        System.out.println("Lamp uitgezet");
-        printLampStatus();
+        if (this.lampStatus == false) {
+            System.out.println("Lamp staat al uit");
+            printLampStatus();
+        } else {
+            this.lampStatus = false;
+            System.out.println("Lamp uitgezet");
+            printLampStatus();
+        }
     }
 
     public void printLampStatus() {
@@ -39,5 +48,11 @@ public class lamp {
         } else {
             System.out.println("Lamp is uit");
         }
+    }
+
+    public static boolean randomLampStatus() {
+        Random random = new Random();
+        boolean status = random.nextBoolean();
+        return status;
     }
 }
