966374c318f1268bdebcc6d984cbb84797e1b37f
tltl111 <tlafeber@outlook.com>
2023-05-27 12:09:14 +0200

    Some class testing

---
+++ src/Student.java
@@ -0,0 +1,26 @@
+class Student {
+    String name;
+    int age;
+    int grade;
+    String school;
+
+    public Student(String name, int age, int grade, String school) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.school = school;
+    }
+
+    public void study() {
+        System.out.println(name + " is studying");
+    }
+
+    public void takeExam() {
+        System.out.println(name + " is taking an exam");
+    }
+
+    public void displayInfo() {
+        System.out.println(name + " is " + age + " years old and in grade "
+                            + grade + " in school " + school);
+    }
+}
