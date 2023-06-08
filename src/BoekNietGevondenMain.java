a5ed823a5a3f9b625dc4c1781208ed31dd3850f4
tltl111 <tlafeber@outlook.com>
2023-06-08 13:13:26 +0200

    New stuff added

---
+++ src/BoekNietGevondenMain.java
@@ -0,0 +1,11 @@
+public class BoekNietGevondenMain {
+    public static void main(String[] args) {
+        BoekNietGevonden boekNietGevonden = new BoekNietGevonden();
+        
+        try {
+            boekNietGevonden.geefLes();
+        } catch (BoekNietGevondenException e) {
+            System.out.println("Boek niet gevonden: " + e.getMessage());
+        }
+    }
+}
